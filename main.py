ed09b61ac1bbb966717009b19b55bd351433ae44
Prajwal <73279106+Prajwal115@users.noreply.github.com>
2022-06-07 01:14:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ base = int(input("Enter the value of the base: "))
 height = int(input("Enter the value of the height: "))
 area = 1/2*base*height
 hypo = math.sqrt(base**2 + height**2)
-
+time.sleep(1)
 print("The area of the triangle is: ",area)
+time.sleep(1)
 print("The hypotenuse of the triangle is: ", hypo)
+print("woohoo! you calculated it!")
